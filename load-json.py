2a5f7660d4b59aa9127789f8ffaed612fd8fed0e
JFong5 <97566318+JFong5@users.noreply.github.com>
2022-11-22 22:41:42 -0700

    Add files via upload

---
+++ load-json.py
@@ -0,0 +1,59 @@
+import pymongo
+from pymongo import MongoClient
+import json
+def inputJsonName():
+    '''
+    Prompts the user for a Json File name
+    Return: Json File Input (Str)
+    '''
+    jsonName = input("Input the json file name you would like to insert. \n")
+    return jsonName
+
+def inputPortNum():
+    '''
+    Prompts the user for a Port Num
+    Return: Port Num Input (Int)
+    '''
+    portNum = int(input("Please input a port number. \n"))
+    return portNum
+
+def main():
+    #Prompt user for portNum and insert it into client name
+    portNum = inputPortNum()
+    client = MongoClient('mongodb://localhost:' + str(portNum))
+
+    #Checks if the database name exists
+    dbs = client.list_database_names()
+    if '291db' in dbs:
+        # if exists removes the database
+        print("Database already exists.")
+        db = client["291db"]
+        db.data.drop()
+        print("Updating database....")
+
+    #Connects database 291db to client and create collection
+    db = client["291db"]
+    collection = db["dplb"]
+
+    #Prompt user for json fileName and process items one row at a time
+    #inserts items into collection_list
+    collection_list = []
+    fileName = inputJsonName()
+    with open(fileName) as file:
+        for item in file:
+            dic = json.loads(item)
+            collection_list.append(dic)
+    
+    #insert collection list into database *deletes collection if it already exists
+    collection.delete_many({})
+    collection.insert_many(collection_list)
+    print("Collection Created!")
+
+    #Indexing testing
+    db.dplb.create_index([("article", "text"),("author", "text")])
+    print(db.dplb.index_information())
+
+
+if __name__ == "__main__":
+    main()
+    
---
+++ phase2.py
@@ -0,0 +1,106 @@
+import pymongo
+from pymongo import MongoClient
+
+def inputPortNum():
+    '''
+    Prompts the user for a Port Num
+    Return: Port Num Input (Int)
+    '''
+    portNum = int(input("Please input a port number. \n"))
+    return portNum
+    
+
+def mainMenu(db):
+    '''
+    Prompts user for an option from 1 to 5
+    Execute certain functions for each choice 
+    '''
+    #Prompt user for option number
+    choice = int(input("Please select the following task you would like to perform: \n1.Search for article \n2.Search for authors \n3.List the venues \n4.Add an article \n5.Exit \n"))
+    if choice == 1:
+        #Executes searchForArticle(db) function
+        searchForArticle(db)
+    elif choice == 2:
+        #Executes searchForAuthors(db) function
+        searchForAuthors(db)
+    elif choice == 3:
+        #Executes listVenues(db) function
+        listVenues(db)
+    elif choice == 4:
+        #Executes addArticle(db) function
+        addArticle(db)
+    elif choice == 5:
+        #Executes exit function and closes program
+        exit()
+
+def searchForArticle(db):
+    '''
+    Prompts user for keyword
+    Displays all Articles matching the keyword
+    '''
+    #Connects to dplb collection in the database
+    collection = db["dplb"]
+
+    #Creates a list to store matchingArticles to keywords
+
+    #Prompts user for matching keyword or keywords
+    userKeywords = input("Please insert a keyword or keywords of the article you would like to search\n")
+    keywordsList = userKeywords.split()
+
+    #Search Query
+    query = {"n_citation": 0}
+    mydoc = collection.find(query)
+
+    print(mydoc.explain()['executionStats'])
+
+    #Reprompt user for user choice
+    userChoice = int(input("Would you like to go back to the main menu or exit? \n1.Go back to main menu \n2.Exit\n"))
+    if userChoice == 1:
+        mainMenu(db)
+    elif userChoice == 2:
+        exit()
+
+def searchForAuthors(db):
+    #User should be able to provide one or more keywords and see all authors whose names contain the keywords
+    #the matches should be case insensitive
+    #For each matching article, display the id, the title, the year, and the venue fields.
+    #the user should be able to select an article to see all fields including the abstract and the authors in addition to the fields listed above.
+    #if the article is referenced by other articles, the id, the title, and the year of those references should be also listed.
+
+    userInput = input("Please enter a keyword to search for authors: ")
+
+    keywordsList = userInput.split()
+    keywords = ""
+
+    for word in keywordsList:
+        keywords += f"\"{word}\" "
+
+    query = collection.find({"authors": { "$elemMatch": { "$regex": "/.*"+keywords+".*/" }}})
+    
+    authorList = []
+    for author in query:
+        authorList.append({"names": author["authors"]})
+    
+    print(authorList)
+
+def listVenues(db):
+    pass
+
+def addArticle(db):
+    pass
+
+def exit():
+    print("Now exiting, thank you for using our services!")
+
+def main():
+    #Prompt user for portNum and insert it into client name
+    portNum = inputPortNum()
+    client = MongoClient('mongodb://localhost:' + str(portNum))
+    db = client["291db"]
+
+    #Calls Main menu function
+    mainMenu(db)
+
+if __name__ == "__main__":
+    main()
+    
