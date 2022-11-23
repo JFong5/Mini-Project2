5895e0a460c72f35eb2b7870aa3a1c71d110a026
JFong5 <97566318+JFong5@users.noreply.github.com>
2022-11-23 15:58:48 -0700

    Update phase2.py

--- phase2.py
+++ phase2.py
@@ -41,7 +41,6 @@ def searchForArticle(db):
     #Connects to dplb collection in the database
     collection = db["dplb"]
 
-    #Creates a list to store matchingArticles to keywords
 
     #Prompts user for matching keyword or keywords
     userKeywords = input("Please insert a keyword or keywords of the article you would like to search\n")
@@ -61,28 +60,52 @@ def searchForArticle(db):
         exit()
 
 def searchForAuthors(db):
-    #User should be able to provide one or more keywords and see all authors whose names contain the keywords
-    #the matches should be case insensitive
-    #For each matching article, display the id, the title, the year, and the venue fields.
-    #the user should be able to select an article to see all fields including the abstract and the authors in addition to the fields listed above.
-    #if the article is referenced by other articles, the id, the title, and the year of those references should be also listed.
+    '''
+    User should be able to provide one or more keywords and see all authors whose names contain the keywords
+    the matches should be case insensitive
+    For each matching article, display the id, the title, the year, and the venue fields.
+    the user should be able to select an article to see all fields including the abstract and the authors in addition to the fields listed above.
+    if the article is referenced by other articles, the id, the title, and the year of those references should be also listed.
+
+    '''
     
+    #Creates Collection
     collection = db["dplb"]
-    userInput = input("Please enter a keyword to search for authors: ")
 
-    keywordsList = userInput.split()
-    keywords = ""
+    #Prompts user for a keyword
+    keyword = input("Please enter a keyword to search for authors: ")
+    keyword = f"\"{keyword}\" "
+    
+    #query to find items in collection with common author names
+    query = [
+            {"$match" : {"$text": {"$search": keyword}}},
+            {"$lookup" :
+                {
+                    "from" : "dplb",
+                    "localField" : "authors", 
+                    "foreignField" : "authors",
+                    "as" : "titlebas_rat"
+                }
+            },
+            {"$match" : {"titlebas_rat.numVotes" : {"$gte" : minvot}}},
+            {"$sort" : {"titlebas_rat.averageRating" : -1}},
+            {"$project" : {"primaryTitle" : 1, "titlebas_rat.numVotes" : 1, "titlebas_rat.averageRating" : 1}}
+    ]
 
-    for word in keywordsList:
-        keywords += f"\"{word}\" "
-
-    query = collection.find({"authors": { "$elemMatch": { "$regex": "/.*"+keywords+".*/" }}})
+    query = collection.find({"authors": { "$elemMatch": { "$regex": "/.*"+keyword+".*/" }}})
     
     authorList = []
     for author in query:
         authorList.append({"names": author["authors"]})
     
     print(authorList)
+
+    #Reprompt user for user choice
+    userChoice = int(input("Would you like to go back to the main menu or exit? \n1.Go back to main menu \n2.Exit\n"))
+    if userChoice == 1:
+        mainMenu(db)
+    elif userChoice == 2:
+        exit()
 
 def listVenues(db):
     '''
@@ -101,8 +124,25 @@ def addArticle(db):
     Adds an article to the collection by providing a unique id, a title, a list of authors, and a year
     Error Handling
     '''
-    #Prompts the user for a unique id, a title, a list of authors and a year
-    uniqueId = input("Please insert a unique id\n")
+    #Connects to dplb collection in the database
+    collection = db["dplb"]
+
+    #Prompts the user for a unique id and checks if the id is unique * Reprompts the user for id until id is unique
+    unique = False
+    while not unique:
+        uniqueId = input("Please insert a unique id\n")
+
+        #Checks if id already exists
+        a = list(collection.find({"id": uniqueId}))
+       
+        #Terminate loop if title exists
+        if len(a) == 0:
+            unique = True
+        else:
+            #reprompts user for id 
+            print("Invalid Input\n")
+
+    #Prompt the user for a title, a list of authors and a year
     title = input("Please insert a title\n")
     authorNames = input("Please inset the names of the authors\n")
     authorList = authorNames.split()
